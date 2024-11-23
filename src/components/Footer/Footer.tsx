c2f38e4f2dce67e5d72195b749e198e5292a8ec5
bykbyk0401 <bykbyk0401@gmail.com>
2024-11-23 23:56:46 +0900

    feat: footer 퍼블리싱

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import { addPostWrapper, footerContainer, homeWrapper, myPageWrapper } from './FooterStyle';
+
+const Footer = () => {
+    return (
+        <div css={footerContainer}>
+            <section css={homeWrapper}>
+                <div></div>
+                <span>홈</span>
+            </section>
+            <section css={addPostWrapper}>
+                <div></div>
+                <span>게시글 추가</span>
+            </section>
+            <section css={myPageWrapper}>
+                <div></div>
+                <span>마이</span>
+            </section>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Footer/FooterStyle.ts
@@ -0,0 +1,43 @@
+import { css } from '@emotion/react';
+
+export const footerContainer = css`
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 1rem;
+`;
+
+export const homeWrapper = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    div {
+        width: 4rem;
+        height: 4rem;
+        background-color: black;
+    }
+`;
+
+export const addPostWrapper = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    div {
+        width: 4rem;
+        height: 4rem;
+        background-color: black;
+    }
+`;
+
+export const myPageWrapper = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    div {
+        width: 4rem;
+        height: 4rem;
+        background-color: black;
+    }
+`;
