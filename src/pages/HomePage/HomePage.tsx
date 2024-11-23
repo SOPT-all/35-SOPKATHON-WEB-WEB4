1ff0941f17916562844e379d59007a79988169ac
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-24 04:14:54 +0900

    feat: HomePage mock data

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,10 @@
 import Card from '@components/Card/Card';
 import RankingSection from '@components/RankingSection/RankingSection';
+import Emoticon from '@components/Emoticon/Emoticon';
+import Header from '@components/Header/Header';
+import Footer from '@components/Footer/Footer';
+import { FAILS_DATA } from '@utils/mocks/failsData';
+import { Key } from 'react';
 
 const rankingData = [
   {
@@ -27,8 +32,33 @@ const rankingData = [
 const HomePage = () => {
   return (
     <div>
+      <Header isGoBack={false} />
       <RankingSection rankingData={rankingData} />
-      <Card content="asefaselifjasliejfl;aisejfl;a" />
+
+      {FAILS_DATA.data.failInfos.map(
+        (fail: {
+          failId: Key | null | undefined;
+          content: string;
+          goodCount: number;
+          drinkCount: number;
+          pellikeonCount: number;
+          talentCount: number;
+        }) => (
+          <div key={fail.failId} style={{ marginBottom: '20px' }}>
+            {/* Card 컴포넌트로 content 표시 */}
+            <Card content={fail.content} />
+            {/* Emoticon 컴포넌트로 클릭된 이모티콘 데이터 표시 */}
+            <Emoticon
+              goodCount={fail.goodCount}
+              drinkCount={fail.drinkCount}
+              pellikeonCount={fail.pellikeonCount}
+              talentCount={fail.talentCount}
+            />
+          </div>
+        )
+      )}
+
+      <Footer />
     </div>
   );
 };
