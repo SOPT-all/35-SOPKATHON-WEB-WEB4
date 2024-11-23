6e3807e4d29c984b271be745eb7ed9da0fdfa73f
imhwarang <imhwarang4@naver.com>
2024-11-24 00:03:59 +0900

    fix: letter-spacing 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -20,99 +20,99 @@ const theme = {
       font-size: 48px;
       font-weight: 700;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     title2_b_40: css`
       font-size: 40px;
       font-weight: 700;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     title3_b_28: css`
       font-size: 28px;
       font-weight: 700;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     title4_b_24: css`
       font-size: 24px;
       font-weight: 700;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
 
     subtitle1_b_20: css`
       font-size: 20px;
       font-weight: 700;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     subtitle2_m_20: css`
       font-size: 20px;
       font-weight: 500;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     subtitle3_b_18: css`
       font-size: 18px;
       font-weight: 700;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     subtitle4_m_18: css`
       font-size: 18px;
       font-weight: 500;
       line-height: normal;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     subtitle5_b_16: css`
       font-size: 16px;
       font-weight: 700;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     subtitle6_b_14: css`
       font-size: 14px;
       font-weight: 700;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
 
     body1_m_16: css`
       font-size: 16px;
       font-weight: 500;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     body2_m_14: css`
       font-size: 14px;
       font-weight: 500;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     body3_b_12: css`
       font-size: 12px;
       font-weight: 700;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     body4_m_12: css`
       font-size: 12px;
       font-weight: 500;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     body5_b_10: css`
       font-size: 10px;
       font-weight: 700;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
     body6_m_10: css`
       font-size: 10px;
       font-weight: 500;
       line-height: 150%;
-      letter-spacing: -2px;
+      letter-spacing: 0px;
     `,
   },
 };
