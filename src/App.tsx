ceb2992ee36ba0ddc3c721922d6a88d6bf084641
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-24 03:42:32 +0900

    중간

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import globalStyle from '@styles/global';
 import theme from '@styles/theme';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import pageRoutes from '@routes/PageRoutes';
-import { HomePage } from './pages';
 function App() {
   const router = createBrowserRouter([...pageRoutes]);
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
-      <HomePage />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
--- src/components/Card/Card.style.ts
+++ src/components/Card/Card.style.ts
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import theme from '@/styles/theme';
+import theme from '@styles/theme';
 
 export const cardWrapper = css`
   margin-top: 1rem;
@@ -8,15 +8,13 @@ export const cardWrapper = css`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding:;
   background-color: ${theme.color.gray200};
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 그림자 효과 */
   text-align: center;
   margin: 0 auto;
 `;
 
 export const content = css`
-  ${theme.font.body1_m_16}
+  font: ${theme.font.body1_m_16}
   line-height: 1.5;
   color: ${theme.color.black};
 `;
