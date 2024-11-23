7968eadd3804a2a6c795d643fd0198ae9cd1b9b8
yoonji Lee <90778858+Leeyoonji23@users.noreply.github.com>
2024-11-23 21:59:29 +0900

    feat: setting globalStyle (#2)
    Co-authored-by: thisishwarang <101498590+thisishwarang@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+function App() {
+  return 
+
 import { ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 
 function App() {
   return <ThemeProvider theme={theme}></ThemeProvider>;
+
 }
 
 export default App;
