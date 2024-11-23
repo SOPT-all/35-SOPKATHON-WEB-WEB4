f96dc730e457498ab2ea5a74c1560eec9d5c5ae3
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-24 03:22:21 +0900

    feat: 디테일 페이지 구현

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,9 +1,23 @@
+import Emoticon from '@components/Emoticon/Emoticon';
 import DetailCard from './components/DetailCard/DetailCard';
+import { DETAIL_FAIL_DATA } from '@utils/mocks/detailFailData';
+import Header from '@components/Header/Header';
+import Footer from '@components/Footer/Footer';
 
 const DetailPage = () => {
+  const cardData = DETAIL_FAIL_DATA.data;
+
   return (
     <div>
-      <DetailCard />
+      <Header isGoBack={true} />
+      <DetailCard content={cardData.content} writerName={cardData.writerName} />
+      <Emoticon
+        goodCount={cardData.goodCount}
+        drinkCount={cardData.drinkCount}
+        pellikeonCount={cardData.pellikeonCount}
+        talentCount={cardData.talentCount}
+      />
+      <Footer />
     </div>
   );
 };
