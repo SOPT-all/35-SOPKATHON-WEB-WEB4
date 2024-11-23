5091a54f27072cb7f8ce6c1075d3b973973c8174
imhwarang <imhwarang4@naver.com>
2024-11-23 23:26:11 +0900

    feat: Header 컴포넌트 1차 완성

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import { headerContainer } from './Header.style';
+
+interface HeaderProps {
+  isGoBack: boolean;
+}
+
+const Header = ({ isGoBack }: HeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    console.log('afe');
+    navigate(-1);
+  };
+  return (
+    <header css={headerContainer}>
+      <section>{isGoBack && <button onClick={handleBackClick}>뒤</button>}</section>
+      <section>로고</section>
+      <section></section>
+    </header>
+  );
+};
+
+export default Header;
