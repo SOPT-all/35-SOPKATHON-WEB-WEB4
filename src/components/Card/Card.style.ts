7851d4442a4c505cf8aab7fb2eae34bdb38ad13d
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-23 23:47:35 +0900

    feat: add Card Component

---
+++ src/components/Card/Card.style.ts
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react';
+import theme from '@/styles/theme';
+
+export const cardWrapper = css`
+  width: 100%;
+  height: 10rem;
+  padding: 0.5rem;
+  background-color: ${theme.color.gray100};
+  text-align: center;
+`;
+
+export const content = css`
+  width: 15rem;
+  margin: 0;
+  color: ${theme.color.gray800};
+  ${theme.font.body1_m_16};
+  line-height: 1.5;
+  height: 3em;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+export const moreButton = css`
+  ${theme.font.subtitle6_b_14}
+  margin-top: 10px;
+  color: ${theme.color.black};
+  font-size: 0.875rem;
+  cursor: pointer;
+  transition: color 0.2s ease;
+
+  &:hover {
+    color: ${theme.color.gray800};
+  }
+`;
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import * as styles from './Card.style';
+
+interface CardProps {
+  content: string;
+  onClickMore?: () => void;
+}
+
+function Card({ content, onClickMore }: CardProps) {
+  return (
+    <div css={styles.cardWrapper}>
+      <p css={styles.content}>{content}</p>
+      <span css={styles.moreButton} onClick={onClickMore}>
+        . . .더보기
+      </span>
+    </div>
+  );
+}
+
+export default Card;
