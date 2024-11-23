bb8171986859789517ed4b32a62d654f431c5805
bykbyk0401 <bykbyk0401@gmail.com>
2024-11-24 02:35:33 +0900

    feat: 페이지 이동 적용

--- src/components/Footer/Footer.style.ts
+++ src/components/Footer/Footer.style.ts
@@ -2,29 +2,47 @@ import { Theme } from '@emotion/react';
 import { css } from '@emotion/react';
 
 export const footerContainer = (theme: Theme) => css`
-    display: flex;
-    justify-content: space-between;
-    padding: 0.8rem 2rem;
-    height: 7rem;
-    background-color: ${theme.color.gray900};
+  display: flex;
+  justify-content: space-between;
+  padding: 0.8rem 2rem;
+  height: 7rem;
+  background-color: ${theme.color.gray900};
 
-    span{
-        color: ${theme.color.gray600};
-        ${theme.font.body4_m_12};
-    }
+  span {
+    color: ${theme.color.gray600};
+    ${theme.font.body4_m_12};
+  }
 `;
 
-export const footerBtnWrapper = css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+export const footerBtnWrapper = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
 
-    &:hover {
-        color: red;
+  &:hover {
+    span {
+      color: ${theme.color.white};
     }
+    path {
+      fill: ${theme.color.white};
+    }
+  }
 `;
 
 export const footerIconStyle = css`
-    width: 3.2rem;
-    height: 3.2rem;
+  width: 3.2rem;
+  height: 3.2rem;
+`;
+
+export const activeFooterTextStyle = (theme: Theme) => css`
+  span {
+    color: ${theme.color.white};
+  }
+`;
+
+export const activeFooterIconStyle = (theme: Theme) => css`
+  path {
+    fill: ${theme.color.white};
+  }
 `;
