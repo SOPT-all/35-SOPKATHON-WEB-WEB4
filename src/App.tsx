4032d8d7e645d8ca7c484393621f464d8011d7c1
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-24 01:47:47 +0900

    feat: App.tsx 복구

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import globalStyle from '@styles/global';
 import theme from '@styles/theme';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import pageRoutes from '@routes/PageRoutes';
-import Emoticon from '@components/Emoticon/Emoticon';
-
 function App() {
   const router = createBrowserRouter([...pageRoutes]);
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
-      <Emoticon />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
