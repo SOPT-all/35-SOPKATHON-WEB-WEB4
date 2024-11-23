deb44ba8ac1764066471db4f7730786331b0cafe
yoonji Lee <90778858+Leeyoonji23@users.noreply.github.com>
2024-11-24 02:02:12 +0900

    [feat]카드 컴포넌트 1차 (#6)
    * feat: add Card Component
    
    * App.tsx 복구
    
    * feat: card component
    
    * feat: emoticon component
    
    * feat:add emoticon component
    
    * feat: add Emoticon Component
    
    * feat: App.tsx 복구
    
    ---------
    
    Co-authored-by: Minha Kang <118591632+m2na7@users.noreply.github.com>

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,7 @@
+export { default as ImgBeer24 } from './ImgBeer24';
+export { default as ImgGood24 } from './ImgGood24';
+export { default as ImgPelican24 } from './ImgPelican24';
+export { default as ImgTalented24 } from './ImgTalented24';
 export { default as IcFailking1 } from './IcFailking1';
 export { default as IcFailking2 } from './IcFailking2';
 export { default as IcFailking3 } from './IcFailking3';
