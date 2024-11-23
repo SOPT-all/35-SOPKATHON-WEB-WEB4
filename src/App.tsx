9fd22885f5cde8f1c78c20376f570416689d40d8
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-23 23:48:16 +0900

    App.tsx 복구

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import globalStyle from '@styles/global';
 import theme from '@styles/theme';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import pageRoutes from '@routes/PageRoutes';
-import Card from '@components/Card/Card';
 
 function App() {
   const router = createBrowserRouter([...pageRoutes]);
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
-      <Card content={''} />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
