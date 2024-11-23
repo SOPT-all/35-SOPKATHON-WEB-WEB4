5954ab890e158e023f90d31fd106e4ff116d1b7c
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-23 22:57:08 +0900

    add index.ts

--- src/routes/PageRoutes.tsx
+++ src/routes/PageRoutes.tsx
@@ -1,27 +1,24 @@
-import { RouteType } from '@type/routeType';
-import HomePage from '@pages/HomePage/HomePage';
-import MyPage from '@pages/MyPage/MyPage';
-import Posting from '@pages/Posting/Posting';
-import DetailPage from '@pages/DetailPage/DetailPage';
-import routhPath from './routhPath';
+import { RouteType } from "@type/routeType";
+import routhPath from "./routhPath";
+import { DetailPage, HomePage, MyPage, Posting } from "@pages/index";
 
-const pageRoutes: RouteType[] = [
-  {
-    path: routhPath.HOME,
-    element: <HomePage />,
-  },
-  {
-    path: routhPath.DETAIL,
-    element: <DetailPage />,
-  },
-  {
-    path: routhPath.MYPAGE,
-    element: <MyPage />,
-  },
-  {
-    path: routhPath.POST,
-    element: <Posting />,
-  },
-];
+const pageRoutes: RouteType[]=[
+    {
+       path:routhPath.HOME,
+       element: <HomePage/>
+    },
+    {
+        path:routhPath.DETAIL,
+        element:<DetailPage/>
+    },
+    {
+        path:routhPath.MYPAGE,
+        element:<MyPage />
+    },
+    {
+        path:routhPath.POST,
+        element:<Posting />
+    }
+]
 
 export default pageRoutes;
