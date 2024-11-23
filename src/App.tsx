017d69366408c154c4babf184432ea41a50743ac
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-24 02:44:32 +0900

    feat: HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import globalStyle from '@styles/global';
 import theme from '@styles/theme';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import pageRoutes from '@routes/PageRoutes';
+import { HomePage } from './pages';
 function App() {
   const router = createBrowserRouter([...pageRoutes]);
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
+      <HomePage />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
